b465a300581d9aa25e0c7d3dab15a93ff81a5f0f
JimmyCADOT <Jimmycadot60150@laposte.net>
2024-01-08 11:53:41 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// Chargement des variables d'environnement 
+// Import des modules nécessaires
+// Import des routes pour l'authentification
+// Import de la configuration de la base de données
+// Initialisation de l'application Express
+// Middleware pour traiter les requêtes JSON 
+// Middleware pour parser les corps de requêtes
+// Utilisation des routes pour l'authentification
+// Configuration des options cors
+// Middleware pour gérer les cors
+// Définition du port du démarrage du serveur 
+// Fonction pour démarrer le serveur 
+// Appel de la fonction pour démarrer le serveur 
